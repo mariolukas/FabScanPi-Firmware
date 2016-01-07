e4fecc484242603904639f29ba93b925d8d1c3b9
Doug LaRue <doug.larue@gmail.com>
2016-01-06 18:10:09 -0800

    Update configuration.h
    added commented out configuration pins for Sanguinololu v1.3a controller board.

--- configuration.h
+++ configuration.h
@@ -12,6 +12,10 @@
 #define LIGHT_PIN A1
 #define RIGHT_LASER_PIN 10
 #define LEFT_LASER_PIN 9
+//Sanguinololu v1.3a
+//#define RIGHT_LASER_PIN 12 //Bed (1284p=p18
+//#define LEFT_LASER_PIN 13  //Hotend (1284p=p19)
+
 #define NUMPIXELS 16
 
 #define MICROSTEP   A5 
@@ -33,6 +37,10 @@
 #define ENABLE_PIN_0  2
 #define STEP_PIN_0    3
 #define DIR_PIN_0     4
+//Sanguinololu v1.3a
+//#define ENABLE_PIN_0  14
+//#define STEP_PIN_0    15
+//#define DIR_PIN_0     21
 
 
 /*************************************************
@@ -44,6 +52,10 @@
 #define ENABLE_PIN_1  5
 #define STEP_PIN_1    6
 #define DIR_PIN_1     7
+//Sanguinololu v1.3a
+//#define ENABLE_PIN_1  26
+//#define STEP_PIN_1    3
+//#define DIR_PIN_1     2
 
 
 /**********************************************
